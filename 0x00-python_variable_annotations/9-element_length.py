1e0b2fce5ae947e916227311028a53f88f908ee2
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-06 22:36:49 +0100

    fix: 9-element_length - re did the function annotations

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,12 +2,12 @@
 """
 Takes a list lst of elements of any type and returns a list of integers.
 """
-from typing import List, Union
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[Union[int, str]]) -> List[int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Return a list of integers representing the lengths of the
     elements of lst
     """
-    return [len(str(x)) for x in lst]
+    return [(i, len(i)) for i in lst]
