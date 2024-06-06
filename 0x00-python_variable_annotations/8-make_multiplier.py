a9a60eba5686b81907be4f54cfd0b37dc1ce976f
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-06 22:20:12 +0100

    update: 8-make_multiplier - fixed the return annotation

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,9 +4,8 @@ Takes a float n as argument and returns a function that multiplies a float by n.
 """
 from typing import Callable
 
-def make_multiplier(multiplier: float) -> Callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies a float by multiplier"""
     def multiply(n: float) -> float:
-        """Return the product of n and multiplier"""
         return n * multiplier
     return multiply
