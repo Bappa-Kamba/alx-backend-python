85f440fed67d684a715b783a630c02b566f72d54
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-06 22:13:48 +0100

    file: 7-to_kv

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Takes a string k and an int OR float v as arguments and returns a tuple.
+The first element of the tuple is the string k. The second element is the square of the number v.
+"""
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple of a string and a float"""
+    return (k, v * v)
