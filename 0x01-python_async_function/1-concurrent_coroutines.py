a1f2a8eadbc2e40af0f45bda96e7bb5c6073b2e4
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-13 18:14:47 +0100

    :wrench: Update wait_n function to return sorted list of delayed floats

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """The basics of async"""
 
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait and return a list of delayed floats"""
     delays = [wait_random(max_delay) for _ in range(n)]
-    return [await delay for delay in delays]
+    return sorted([await delay for delay in delays])
