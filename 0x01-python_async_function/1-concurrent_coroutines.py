e1a317a7613ff0a31d8e404e1540e02f9d350dad
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-13 18:05:52 +0100

    chore: conformed to PEP8 standards

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,6 +3,7 @@
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> list:
     """Wait and return a list of delayed floats"""
     delays = [wait_random(max_delay) for _ in range(n)]
