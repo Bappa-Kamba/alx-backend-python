ab1e81bc2846c7007792646f3b822233dd7ae9c9
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-13 18:10:47 +0100

    chore: Update wait_random function to use int type for max_delay parameter

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: float = 10) -> float:
-    wait = random.uniform(0.0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    wait = random.uniform(0, max_delay)
     await asyncio.sleep(wait)
     return wait
