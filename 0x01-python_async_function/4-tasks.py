604eb8577b08dd2e0dbbd9798e7ba0005ebb7c4b
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-13 18:23:46 +0100

    :hammer: Update task_wait_n function to return sorted list of delayed floats

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,16 +2,12 @@
 """The basics of async"""
 
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Wait and return a list of delayed floats"""
     delays = [task_wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in delays]
-
-
-n = 5
-max_delay = 6
-print(asyncio.run(task_wait_n(n, max_delay)))
