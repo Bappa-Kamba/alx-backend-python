72bf3aead7696b7e0217d186bb189b1704124e53
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-13 19:45:23 +0100

    Update measure_runtime function to use asyncio.gather with a comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,10 +10,5 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure the runtime"""
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time() - start
