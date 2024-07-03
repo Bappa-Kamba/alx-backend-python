35b725f9033c8e511435aa8e7e49a2d7a66a8482
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-03 22:23:56 +0100

    :rotating_light: "Added test cases for get_json function and refactored test_access_nested_map method"

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Utils Test Module """
 import unittest
+import unittest.mock
+import requests
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Mapping, Sequence, Any
 
 
@@ -16,7 +18,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
+    def test_access_nested_map(
+        self, nested_map: Mapping,
+        path: Sequence, expected: Any
+    ):
         """
             Tests that the `utils.access_nested_map` method functions
             correctly.
@@ -37,4 +42,28 @@ class TestAccessNestedMap(unittest.TestCase):
             exception.
         """
         with self.assertRaises(KeyError):
-            self.assertEqual(access_nested_map(nested_map, path), expected) 
+            self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+        Test class to test `utils.get_json` method, inherits from
+        `unittest.Testcase`.
+    """
+    @unittest.mock.patch('requests.get')
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url: str, expected):
+        """
+            Tests that the `utils.get_json` method returns the correct
+            response.
+        """
+        mock_response = unittest.mock.Mock()
+        mock_response.json.return_value = expected
+        requests.get.return_value = mock_response
+
+        requests.get.assert_called_once_with(url)
+
+        self.assertEqual(get_json(url), expected)
