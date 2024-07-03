09b422c859ffbffc923295a1503533bf9faa8e83
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-03 22:09:15 +0100

    :tada: implementation of access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,3 +23,18 @@ class TestAccessNestedMap(unittest.TestCase):
 
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a'), KeyError('a')),
+        ({'a': 1}, ('a', 'b'), KeyError('b'))
+    ])
+    def test_access_nested_map_exception(
+        self, nested_map: Mapping,
+        path: Sequence, expected: Any
+    ):
+        """
+            Tests that the `utils.access_nested_map` method raises the correct
+            exception.
+        """
+        with self.assertRaises(KeyError):
+            self.assertEqual(access_nested_map(nested_map, path), expected) 
