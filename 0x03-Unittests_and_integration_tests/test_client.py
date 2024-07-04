3eac01aa0c3278a57b339e3ce1532c226ae19c04
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-04 07:23:39 +0100

    :rotating_lights: "Updated test_client.py: added import of PropertyMock, modified test_org method, and added test_public_repos_url method"

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Client Tests Module """
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -16,9 +16,33 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_org(self, org, expected, mock_get_json):
         """ Tests `client.GithubOrgClient.org` returns expected results """
         mock_get_json.return_value = expected
-        
+
         test_client = GithubOrgClient(org)
 
         self.assertEqual(test_client.org, expected)
-        
-        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org}')
+
+        mock_get_json.assert_called_once_with(
+            f'https://api.github.com/orgs/{org}')
+
+    def test_public_repos_url(self):
+        """ Tests `client.GithubOrgClient.public_repos_url` returns
+            expected results
+        """
+        expected_payload = {
+            'login': 'abc',
+            'repos_url': 'https://api.github.com/orgs/abc/repos'
+        }
+        with patch.object(
+            GithubOrgClient,
+            '_public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_public:
+            mock_public.return_value = expected_payload
+
+            test_client = GithubOrgClient('abc')
+
+            self.assertEqual(
+                test_client._public_repos_url,
+                expected_payload
+            )
+
